5a6b723109bdbda17a14316634846df0b453ab08
Bjørge Næss <bjoerge@bengler.no>
2013-01-23 15:47:40 +0100

    Run npm install from spec helper

--- extconf.rb
+++ extconf.rb
@@ -3,9 +3,7 @@ require 'open3'
 
 create_makefile("Dummy")
 
-['npm install', 'npm shrinkwrap'].each do |cmd|
-  puts "* Running #{cmd}"
-  Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
-    fail "Error running '#{cmd}': #{stderr.read}" unless wait_thr.value.success?
-  end
+puts "* Running npm install"
+Open3.popen3('npm install') do |stdin, stdout, stderr, wait_thr|
+  fail "Error running 'npm install': #{stderr.read}" unless wait_thr.value.success?
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,13 @@
+require 'open3'
+
+unless File.exists?("./node_modules")
+  # Run npm install to make sure we get all needed node modules
+  puts "* Running npm install"
+  Open3.popen3('npm install') do |stdin, stdout, stderr, wait_thr|
+    fail "Error running 'npm install': #{stderr.read}" unless wait_thr.value.success?
+  end
+end
+
 require 'bundler'
 require 'simplecov'
 require 'rack/test'
