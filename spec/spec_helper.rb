94518051aeb088f697f75e0a9ad2a02e5f9abd4b
Bjørge Næss <bjoerge@bengler.no>
2013-10-22 00:11:37 +0200

    Fix failing tests by shrinkwrapping demo project

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,10 @@
 require 'open3'
 
-unless File.exists?("./node_modules")
-  # Run npm install to make sure we get all needed node modules
-  puts "* Running npm install"
-  Open3.popen3('npm install') do |stdin, stdout, stderr, wait_thr|
-    fail "Error running 'npm install': #{stderr.read}" unless wait_thr.value.success?
+# Run npm install to make sure we get all needed node modules
+['./', './spec/fixtures/'].each do |dir|
+  puts "* Running npm install in #{dir}"
+  Open3.popen3('npm install', {chdir: dir}) do |stdin, stdout, stderr, wait_thr|
+    fail "Error running 'npm install' in #{dir}: #{stderr.read}" unless wait_thr.value.success?
   end
 end
 
